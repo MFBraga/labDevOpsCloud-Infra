8f5d7e29dc5e579c07a17c8251bb48f1629e96ad
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2022-05-27 23:21:27 -0300

    Compute Engine Create

---
+++ gcp-main.tf
@@ -0,0 +1,22 @@
+# Cria uma VM no Google Cloud
+resource "google_compute_instance" "firstvm" {
+  name         = "helloworld"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-c"
+
+  # Defini a Imagem da VM
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20220303a"
+    }
+  }
+
+  # Habilita rede para a VM com um IP público
+  network_interface {
+    network = "default" # Estamos usando a VPC default que já vem por padrão no projeto.
+
+    access_config {
+    // A presença do bloco access_config, mesmo sem argumentos, garante que a instância estará acessível pela internet.
+    }
+  }
+}
---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.22.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpkey.json")
+
+  project = "teste-351601"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
