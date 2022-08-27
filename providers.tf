55dd5742b25faabfd7d6cd0a379b8bdcd9a0e7a5
MFBraga <mauricio.braga@aluno.faculdadeimpacta.com.br>
2022-08-27 13:58:27 -0300

    GCP KEY to TERRAFORM VAR

--- providers.tf
+++ providers.tf
@@ -9,9 +9,6 @@ terraform {
 
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
-
-  credentials = file("/home/mauricio_braga/gcpkey.json")
-
   project = "${var.gcp_project_id}"
   region  = "${var.gcp_region}"
   zone    = "${var.gcp_zone}"
